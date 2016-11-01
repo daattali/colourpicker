f1f5169bfcaa0de557cced61a69ad88c273fcc23
Dean Attali <daattali@gmail.com>
2016-11-01 15:55:43 -0400

    add keyboard shortcuts for spacebar, number keys, delete

--- inst/gadgets/colourpicker/js/shinyjs-funcs.js
+++ inst/gadgets/colourpicker/js/shinyjs-funcs.js
@@ -20,15 +20,35 @@ shinyjs.init = function() {
     if ($(event.target).closest(".shiny-input-container").length > 0) {
       return;
     }
+
+    // Escape is already taken care of by shiny
+
+    // Enter
     if (event.which == 13) {
       $("#done").click();
     }
 
+    // Left/right arrows
     if (event.which == 37) {
       Shiny.onInputChange("jsColNav", [-1, Math.random()]);
     }
     if (event.which == 39) {
       Shiny.onInputChange("jsColNav", [1, Math.random()]);
+    }
+
+    // Spacebar
+    if (event.which == 32) {
+      $("#addColBtn").click();
+    }
+
+    // Delete
+    if (event.which == 46) {
+      $("#removeColBtn").click();
+    }
+
+    // Number keys
+    if (event.which >= 49 && event.which <= 57) {
+      Shiny.onInputChange("jsColNum", [event.which-48, Math.random()]);
     }
   });
 };
